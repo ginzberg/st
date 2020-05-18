fc9da2f354d84fa5c18d8c1e6af919cf5cb82723
ginzberg <rcallen@gmail.com>
2020-05-18 11:55:54 -0700

    config.def.h

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "IBM Plex Mono";
+static char *font = "IBM Plex Mono:size=14";
 static int borderpx = 2;
 
 /*
